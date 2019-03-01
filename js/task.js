f4f07887490de2a9a77adcf1d842f63168229e2b
Bahersaad676 <bahersaad676@gmail.com>
2019-03-01 04:08:47 -0800

    Update task.js

--- js/task.js
+++ js/task.js
@@ -3,16 +3,29 @@ btn = document.getElementById('btn'),
 goalAdded = document.getElementById('gls'),
 glsChild = [...document.getElementById('gls').children],
 theGoals = [...document.getElementsByClassName('the-goal')],
-lengthGoals = document.getElementById('lengthGolas');
+lengthGoals = document.getElementById('lengthGolas'),
+achive = document.getElementById('achive'),
+inputErr = document.getElementById('input-err'),
+removed = [];
 btn.onclick = function() {
-	var d = document.createElement("div");
+	createElem();
+	rempveGoals();
+}
+
+function createElem() {
+	if (input.value === "") {
+		inputErr.textContent = "you have to enter your goals";
+	} else {
+		var d = document.createElement("div");
 	d.textContent = input.value;
 	d.classList.add("the-goal");
 	goalAdded.append(d);
 	input.value = "";
-	rempveGoals();
+	inputErr.textContent = "";
+	}
 	
 }
+
 function rempveGoals() {
 	var cccc = [...goalAdded.children];
 	console.log(cccc);
@@ -20,6 +33,9 @@ function rempveGoals() {
 	for (var i = 0; i < cccc.length; i++) {
 	cccc[i].onclick = function () {
 		this.remove();
+		removed.push(this.remove());
+		console.log(removed.length);
+		achive.textContent = removed.length;
 		}
 	}
 }
